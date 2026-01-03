d4bfd2120406f8bc877030f4f115f75601260b38
AhmetAktas67 <ahmet67aktas@gmail.com>
2026-01-03 18:06:40 +0100

    Erstes Oberfläche Design von Menü und Viewmodels + Binding der Datenbank,

---
+++ Implementierung/SchulbibliothekWpf/SchulbibliothekWpf/Models/BuchAnzeige.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchulbibliothekWpf.Models
+{
+    public class BuchAnzeige
+    {
+        public string Titel { get; set; } = "";
+        public string Autor { get; set; } = "";
+        public string ISBN { get; set; } = "";
+        public int Erscheinungsjahr { get; set; }
+
+        public string AktuellerStand { get; set; } = "";
+        public string Nutzername { get; set; } = "";
+    }
+}
---
+++ Implementierung/SchulbibliothekWpf/SchulbibliothekWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using SchulbibliothekWpf.Data;
+using SchulbibliothekWpf.Models;
+
+namespace SchulbibliothekWpf.ViewModels
+{
+    public class MainWindowViewModel
+    { 
+       public ObservableCollection<BuchAnzeige> Buecher {  get; } = new ObservableCollection<BuchAnzeige>();
+
+        public MainWindowViewModel() 
+        {
+            using var db = new BibliothekContext();
+
+            foreach (var b in db.Buecher)
+            {
+                Buecher.Add(new BuchAnzeige
+                {
+                    Titel = b.Titel,
+                    Autor = b.Autor,
+                    ISBN = b.ISBN,
+                    Erscheinungsjahr = b.Erscheinungsjahr,
+
+                     AktuellerStand = "Verfügbar",
+                    Nutzername = "-"
+                });
+            }
+        }
+    }
+}
